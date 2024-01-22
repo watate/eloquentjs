a3d7dbff418f9a9326b2dd615be39b6f83d41c80
Walter Tay Ann Lee <40448679+watate@users.noreply.github.com>
2024-01-22 21:28:46 +0800

    Solved nth

--- four.js
+++ four.js
@@ -35,11 +35,23 @@ function prepend(element, list) {
     }
 }
 
+function nth(list, index) {
+    // arr = listToArray(list);
+    // if (index > arr.length - 1) return undefined
+    // else return arr[index];
+
+    //Recursive version
+    if (list === null) return undefined;
+    else if (index === 0) return list.value;
+    else return nth(list.rest, index - 1);
+}
+
 console.log(arrayToList([10, 20, 30]));
 // → {value: 10, rest: {value: 20, rest: null}}
 console.log(listToArray(arrayToList([10, 20, 30])));
 // // → [10, 20, 30]
 console.log(prepend(10, prepend(20, null)));
 // // → {value: 10, rest: {value: 20, rest: null}}
-// console.log(nth(arrayToList([10, 20, 30]), 1));
+console.log(nth(arrayToList([10, 20, 30]), 1));
 // → 20
+console.log(nth(arrayToList([10, 20, 30]), 3));
