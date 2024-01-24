53e5b861f1858740d1e8d2480fd2b2189ea88eb8
Walter Tay Ann Lee <40448679+watate@users.noreply.github.com>
2024-01-24 12:58:10 +0800

    Solved dominant writing direction

--- five.js
+++ five.js
@@ -13,14 +13,23 @@
 // loop(3, n => n > 0, n => n - 1, console.log);
 
 // EVERYTHING
-function every(array, test) {
-    if (array.length === 0) return true
-    return array.map(test).reduce((a, b) => a && b);
-}
+// function every(array, test) {
+//     if (array.length === 0) return true
+//     return array.map(test).reduce((a, b) => a && b);
+// }
   
-  console.log(every([1, 3, 5], n => n < 10));
-  // → true
-  console.log(every([2, 4, 16], n => n < 10));
-  // → false
-  console.log(every([], n => n < 10));
-  // → true
+//   console.log(every([1, 3, 5], n => n < 10));
+//   // → true
+//   console.log(every([2, 4, 16], n => n < 10));
+//   // → false
+//   console.log(every([], n => n < 10));
+//   // → true
+
+// Dominant writing direction
+function dominantDirection(text) {
+    // Your code here.
+    return countBy(text, char => {
+         script = characterScript(char.codePointAt(0));
+      return script ? script.direction : "none";
+    }).reduce((a, b) => a.count > b.count ? a.name : b.name);
+  }
