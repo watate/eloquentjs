9ddbd84ae257a93d3b2fd076472df3abb98de7fe
Walter Tay Ann Lee <40448679+watate@users.noreply.github.com>
2024-01-24 11:53:51 +0800

    Solved yourOwnLoop

--- five.js
+++ five.js
@@ -1,3 +1,13 @@
-let arrays = [[1, 2, 3], [4, 5], [6]];
-newArrays = arrays.reduce((a, b) => a.concat(b));
-console.log(newArrays);
+// FLATTENING
+// let arrays = [[1, 2, 3], [4, 5], [6]];
+// newArrays = arrays.reduce((a, b) => a.concat(b));
+// console.log(newArrays);
+
+// YOUR OWN LOOP
+function loop(value, testFunc, updateFunc, bodyFunc) {
+    for (let i = value; testFunc(i); i = updateFunc(i)) {
+        bodyFunc(i);
+    }
+}
+
+loop(3, n => n > 0, n => n - 1, console.log);
