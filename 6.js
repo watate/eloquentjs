04a8524ab53d3b4736b3006eae8a20ca57ab69a3
Walter Tay Ann Lee <40448679+watate@users.noreply.github.com>
2024-01-25 14:47:00 +0800

    Solved Vec

---
+++ 6.js
@@ -0,0 +1,25 @@
+class Vec {
+    constructor (x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    plus(a) {
+        return new Vec(this.x + a.x, this.y + a.y);
+    }
+
+    minus(a) {
+        return new Vec(this.x - a.x, this.y - a.y);
+    }
+
+    get length() {
+        return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+    }
+}
+
+console.log(new Vec(1, 2).plus(new Vec(2, 3)));
+// → Vec{x: 3, y: 5}
+console.log(new Vec(1, 2).minus(new Vec(2, 3)));
+// → Vec{x: -1, y: -1}
+console.log(new Vec(3, 4).length);
+// → 5
