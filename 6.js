14dd4a88c9c96da6d6e14e78cf6ba8af5d530f86
Walter Tay Ann Lee <40448679+watate@users.noreply.github.com>
2024-01-25 15:41:36 +0800

    Solved Groups

--- 6.js
+++ 6.js
@@ -1,25 +1,67 @@
-class Vec {
-    constructor (x, y) {
-        this.x = x;
-        this.y = y;
+// class Vec {
+//     constructor (x, y) {
+//         this.x = x;
+//         this.y = y;
+//     }
+
+//     plus(a) {
+//         return new Vec(this.x + a.x, this.y + a.y);
+//     }
+
+//     minus(a) {
+//         return new Vec(this.x - a.x, this.y - a.y);
+//     }
+
+//     get length() {
+//         return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+//     }
+// }
+
+// console.log(new Vec(1, 2).plus(new Vec(2, 3)));
+// // → Vec{x: 3, y: 5}
+// console.log(new Vec(1, 2).minus(new Vec(2, 3)));
+// // → Vec{x: -1, y: -1}
+// console.log(new Vec(3, 4).length);
+// // → 5
+
+class Group {
+    constructor() {
+        this.group = [];
     }
 
-    plus(a) {
-        return new Vec(this.x + a.x, this.y + a.y);
+    add(x) {
+        if (!this.group.includes(x)) {
+            this.group.push(x);
+        }
     }
 
-    minus(a) {
-        return new Vec(this.x - a.x, this.y - a.y);
+    delete(x) {
+        if (this.group.includes(x)) {
+            this.group.splice(this.group.indexOf(x), 1);
+        }
     }
 
-    get length() {
-        return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+    has(x) {
+        return this.group.includes(x);
+    }
+
+    static from(iterable) {
+        let output = new Group();
+        for (let x of iterable) {
+            output.add(x);
+        }
+        return output;
     }
 }
 
-console.log(new Vec(1, 2).plus(new Vec(2, 3)));
-// → Vec{x: 3, y: 5}
-console.log(new Vec(1, 2).minus(new Vec(2, 3)));
-// → Vec{x: -1, y: -1}
-console.log(new Vec(3, 4).length);
-// → 5
+let group = Group.from([10, 20]);
+console.log(group.has(10));
+// // → true
+console.log(group.has(30));
+// // → false
+console.log(group);
+group.add(10);
+group.delete(10);
+console.log(group.has(10));
+// // // → false
+console.log(group);
